8b44f3b7349470d354b47afec0dd401b18d9ab34
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-02-17 19:20:31 +0100

    Added a test connection to marvel.com

---
+++ js/test.js
@@ -0,0 +1,21 @@
+"use strict";
+const privateKey = "b007589db8518dcbb941ed7bdef21b3840498b0a";
+const publicKey = "b52df188d35ef1bbeef3878e0342661c";
+let newTime = new Date().getTime();
+let hash = CryptoJS.MD5(newTime + privateKey + publicKey).toString();
+
+function comicUrl() {
+    const url = new URL("http://gateway.marvel.com/v1/public/characters");
+    
+    url.searchParams.append('ts', newTime);
+    url.searchParams.append('apikey', publicKey);
+    url.searchParams.append('hash', hash);
+    url.searchParams.append('name', "iron man");
+    
+
+    return url;
+}
+
+document.querySelector("#test").onclick = async function() {
+    await fetch(comicUrl()).then(response => console.log(response));
+}
