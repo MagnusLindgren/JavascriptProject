ebac544ed629b4ebad6977ec8966e32f01a3217c
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-02-18 13:14:37 +0100

    Wrote some todos in main.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/* Sökfunktion */
+
+/* Url byggare */
+
+/* Funktion för att hämta data från API (fetch) */
+
+/* Skapa innehåll till sökresultat */ 
+
+/* Funktion för att radera sökresultat */
+
+/* Funktion för att spara via localStorage */
