647ce4fc0b6d5e3439428f4d515f9dc132d90a5f
Marcus Backholm <marcus.backholm@live.se>
2021-02-24 13:49:53 +0100

    class name for card above the scrollbar

---
+++ js/marcus.js
@@ -0,0 +1,15 @@
+// Public key: cb4caffe743bf27799e98341a711c25f
+// Private key: a2772e164899250bf624c834394e5fc6f320ff35
+
+const button = document.querySelector('#button'); // Vid knapptryck.
+button.addEventListener('click', event => {
+    HeroSelect
+})
+
+const inputSearch = document.querySelector('.searchbar') // Sökfältet
+
+//funktion för sökning utav hjälte.
+function HeroSelect( search ) {
+var key = 'cb4caffe743bf27799e98341a711c25f';
+fetch('https://gateway.marvel.com/v1/public/characters'+ searchbar.value + )
+}
