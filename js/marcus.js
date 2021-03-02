72a7e25d6dcce724e07087c7d2561e9b274d904d
Marcus Backholm <Marcus.Backholm@live.se>
2021-03-02 16:47:34 +0100

    börjar om med JS

--- js/marcus.js
+++ js/marcus.js
@@ -2,28 +2,21 @@
 // Private key: a2772e164899250bf624c834394e5fc6f320ff35
 
 "use strict";
-const privateKey = "a2772e164899250bf624c834394e5fc6f320ff35";
-const publicKey = "cb4caffe743bf27799e98341a711c25f";
-let newTime = new Date().getTime();
-let hash = CryptoJS.MD5(newTime + privateKey + publicKey).toString();
+const PublKey = 'cb4caffe743bf27799e98341a711c25f';
+const privKey = 'cb4caffe743bf27799e98341a711c25f';
 
-function comicUrl() {  //Hämtar information genom API call på hjältar.
-    const url = new URL("https://gateway.marvel.com/v1/public/characters");
-    
-    url.searchParams.append('ts', newTime);
-    url.searchParams.append('apikey', publicKey);
-    url.searchParams.append('hash', hash);
-    url.searchParams.append('name', "Spider-Man");
-    
+const getPostPromise = fetch('https://gateway.marvel.com/') // Tar ett argument.
 
-    return url;
-}
-// Knapp funktion. 
-document.querySelector("#button").onclick = async function() {
-    const response = await fetch(comicUrl());
-    const data = await response.json();
-    console.log(data);
-}
+getPostPromise
+.then(data => data.json())
+.then(data => {
+    data.posts.foreach((item) => {
+        console.log(item.titel); // Hämtar specifikt element i API:N
+    });
+})
+.catch((err) => {  // Skickar ut error meddelande om det blir fel.
+    console.log(err)
+})
 
 // Få searchbar att ta input och skriva ut bild samt information om hjälten.
 
