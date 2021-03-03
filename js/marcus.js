6588a330ebe567341e357b3c4354e6c8d03ec341
Marcus Backholm <Marcus.Backholm@live.se>
2021-03-03 16:11:10 +0100

    ändrat lite i HTML och byggt JS för att hämta val av hjälte samt bild. test pågår

--- js/marcus.js
+++ js/marcus.js
@@ -1,34 +1,44 @@
-// Public key: cb4caffe743bf27799e98341a711c25f
-// Private key: a2772e164899250bf624c834394e5fc6f320ff35
+// class="heroName" Hero: WANDA MAXIMOF Hjältens namn skall skriva ut här.
+// class="informationHero" Information om hjälten skall skriva ut här.
+// class="searchbar" sökfunktion.
+// id="button" knappfunktion.
+
+document.addEventListener("DOMcontentLoaded", heroBind);
+const publicKey = "cb4caffe743bf27799e98341a711c25f";
+const privateKey = "a2772e164899250bf624c834394e5fc6f320ff35";
+
+function heroBind(event){
+
+    const number = event.timestamp;
+    const req = new XMLHttpRequest();
+    const website = 'https://gateway.marvel.com/v1/public/characters?name='
+    const name = document.getElementById('heroName').value;
+    const web = website + name + "ts=" + number + '&apikey=' + publicKey + "&hash=" + privateKey;
+
+    document.getElementById('button').addEventListener('click', function (event){
+    document.getElementById('searchbar').textContent = "";
+    document.getElementById('informationHero').textContent = "";
+    });
+    req.open('GET', web, true);
+
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.setRequestHeader('load', function(){
+        if(req.status >= 200 && req.status < 400) {
+            const result = JSON.parse(req.responseText);
+            document.getElementById('heroName').textContent = result.heroName;
+            document.getElementById('informationHero').textContent = result.informationHero;
+            const img = document.createElement('img'); // Hämtar bild för val av hjälte input.
+            img.src = result.thumbnail.path + '/landscape_medium.jpg';
+            document.getElementById('pic').appendChild(img)
+
+        }
+
+    event.preventDefault();
+    });
+    req.send(null);
 
 
-"use strict";
-const PublicKey = 'cb4caffe743bf27799e98341a711c25f';
-const privateKey = 'a2772e164899250bf624c834394e5fc6f320ff35';
-let newTime = new Date().getTime();
-let hash = CryptoJS.MD5(newTime + privateKey + PublicKey).toString();
-
-// Funktion för att hämta specifik marvel hjälte.
-function GetCharacter(){
-    const url = new URL ('https://gateway.marvel.com/v1/public/characters')
-
-    url.searchParams.append('ts', newTime);
-    url.searchParams.append('apikey', PublicKey);
-    url.searchParams.append('hash', hash)
-    url.searchParams.append('name', "iron man");
-
-    return url;
-};
-
-// Knapp funktion som hämtar resultatet ifrån APIN.
-document.querySelector('#button').onclick = async function() {
-    const response = await fetch(GetCharacter());
-    const data = await response.json();
-    console.log(data);
-    return data;
 }
-
-const searchBox = document.querySelector('.searchbar')
 
 
 // Få searchbar att ta input och skriva ut bild samt information om hjälten.
@@ -36,9 +46,4 @@ const searchBox = document.querySelector('.searchbar')
 /*Funktion Superhjältar
 - bild på hjälten
 - information om hjälten
-- skapare */
-
-/* funktion Comics
-- bild serietidningen
-- information om serietidningen
 - skapare */
