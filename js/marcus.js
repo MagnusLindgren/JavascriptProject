f8d60f94ce0d5532a678ac7d10c2d840da397006
Marcus Backholm <Marcus.Backholm@live.se>
2021-03-03 18:09:40 +0100

    Fixat så man kan söka på hjälte och få upp bild på det övre kortet!

--- js/marcus.js
+++ js/marcus.js
@@ -3,42 +3,112 @@
 // class="searchbar" sökfunktion.
 // id="button" knappfunktion.
 
-document.addEventListener("DOMcontentLoaded", heroBind);
-const publicKey = "cb4caffe743bf27799e98341a711c25f";
-const privateKey = "a2772e164899250bf624c834394e5fc6f320ff35";
+// document.addEventListener("DOMContentLoaded", heroBind);
+var publicKey = "cb4caffe743bf27799e98341a711c25f";
+var privateKey = "a2772e164899250bf624c834394e5fc6f320ff35";
+document.getElementById('button').addEventListener('click', function (event) {
+    console.log("click");
+    var searchButton = document.getElementById("searchbar");
+    document.getElementById('heroName').textContent = "Hero: ";
+    document.getElementById('informationHero').textContent = "Information: ";
 
-function heroBind(event){
-
-    const number = event.timestamp;
-    const req = new XMLHttpRequest();
-    const website = 'https://gateway.marvel.com/v1/public/characters?name='
-    const name = document.getElementById('heroName').value;
-    const web = website + name + "ts=" + number + '&apikey=' + publicKey + "&hash=" + privateKey;
-
-    document.getElementById('button').addEventListener('click', function (event){
+    findHero(searchButton.value, event); 
     document.getElementById('searchbar').textContent = "";
-    document.getElementById('informationHero').textContent = "";
-    });
-    req.open('GET', web, true);
+});
+
+
+function findHero(query, event) {
+
+    var website = 'https://gateway.marvel.com/v1/public/characters';
+
+    console.log("do hash");
+    var ts = event.timeStamp;
+    var hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
+    console.log(hash);
+    var url = website;
+    $.getJSON(url, {
+        ts: ts,
+        apikey: publicKey,
+        hash: hash,
+        name: query
+    })
+        .done(function (response) {
+            console.log(response);
+            var result = response.data.results[0];
+            if (result != null) {
+                console.log("WE HAVE A RESULT!!!");
+                console.log(result);
+                document.getElementById('searchCard').style.visibility = 'visible';
+                document.getElementById('heroName').textContent = result.name;
+                document.getElementById('informationHero').textContent = result.description;
+                var img = result.thumbnail.path + "." + result.thumbnail.extension;
+                document.getElementById('topImage').src = img;
+                document.getElementById('topImage').style.visibility = 'visible';
+            } else {
+                console.log("we do not have a result :(((((((((((((((((((((")
+                document.getElementById('heroName').textContent = 'NOT FOUND FFS - Search better!';
+                document.getElementById('topImage').src = "";
+                document.getElementById('topImage').style.visibility = 'hidden';
+                document.getElementById('informationHero').textContent = '';
+            }
+
+   
+        })
+        .fail(function (err) {
+            // the error codes are listed on the dev site
+            console.log(err);
+        });
+}
+
+  
+/*function findHero2(query, event) {
+  
+    var website = 'https://gateway.marvel.com/v1/public/characters';
+
+    var req = new XMLHttpRequest();
+        console.log("do hash");
+    var ts = event.timeStamp;
+    var hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
+        var url = website + "?name=" + query + "&ts=" + ts + '&apikey=' + publicKey + "&hash=" + hash;
+    console.log(url);
+
+    req.open('GET', url, true);
 
     req.setRequestHeader('Content-Type', 'application/json');
-    req.setRequestHeader('load', function(){
-        if(req.status >= 200 && req.status < 400) {
-            const result = JSON.parse(req.responseText);
-            document.getElementById('heroName').textContent = result.heroName;
-            document.getElementById('informationHero').textContent = result.informationHero;
-            const img = document.createElement('img'); // Hämtar bild för val av hjälte input.
-            img.src = result.thumbnail.path + '/landscape_medium.jpg';
-            document.getElementById('pic').appendChild(img)
-
+    req.addEventListener('load', function () {
+        console.log("load event triggered");
+        if (req.status >= 200 && req.status < 400) {
+            console.log("decent status code");
+            var response = JSON.parse(req.responseText);
+            var result = response.data.results[0];
+            console.log(result);
+        
+           if (result != null) {
+                console.log("WE HAVE A RESULT!!!");
+                console.log(result);
+                document.getElementById('searchCard').style.visibility = 'visible';
+                document.getElementById('heroName').textContent = result.name;
+                document.getElementById('informationHero').textContent = result.description;
+                var img = result.thumbnail.path + "." + result.thumbnail.extension;
+                document.getElementById('topImage').src = img;
+                document.getElementById('topImage').style.visibility = 'visible';
+            } else {
+                console.log("we do not have a result :(((((((((((((((((((((")
+                document.getElementById('heroName').textContent = 'NOT FOUND FFS - Search better!';
+                document.getElementById('topImage').src = "";
+                document.getElementById('topImage').style.visibility = 'hidden';
+                document.getElementById('informationHero').textContent = '';
+            }
         }
 
-    event.preventDefault();
+        event.preventDefault();
     });
+    console.log("Sending request");
     req.send(null);
+};
 
 
-}
+
 
 
 // Få searchbar att ta input och skriva ut bild samt information om hjälten.
